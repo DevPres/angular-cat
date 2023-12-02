c3a61544515c6005e5fb038fe0f5c499fabf0d37
Dario Presutti <dariopresde@protonmail.com>
2023-12-02 11:00:00 +0100

    test new prompt

--- angular_magic_component.py
+++ angular_magic_component.py
@@ -2,35 +2,36 @@ def angular_magic_component_factory(input):
     return f'''
     This is the user request: '{input}'. 
 
-    In the request there are some string delimited by single quotes, for example 'form'. this are magic keys
-    In the request there is a string prefixed by -, for example -cat-pur. This is the magic name. If magic name is not present in the request, the magic name is ALWAYS cat-pur. if there are multiple name, take ONLY the first.
-    In the request there is a string prefixed by --, for example --app. This is the prefix. If the  is not present in the request, the prefix is ALWAYS --app. If there are multiple name, take ONLY the first.
+    In the request there are strings that are keys. these are magick_keys. request example: 'generate angular component with keys: form,signal ....'. in this example form and signal are magic_keys.
+    In the request there is a string that is the name. this is the magic_name. request example: 'generate angular component with name cat-template'. in this example cat-template is the magic_name. if there isn't a name the name is ALWAYS cat-template
+    In the request there is a string that is the prefix. this is the magic_prefix. request example 'generate angular component with prefix app'. in this example app is the magic_prefix. if there isn't a prefix the prefix is ALWAYS app
+    The magic_name_capitalized is equal to the magic name but in PascalCase and ends with Component. example if magic_name is cat-pur the magic_name_capitalized is CatPurComponent.
 
-    the magic name capitalized is equl to the magic name but in PascalCase and ends with Component. example if magic name is cat-pur the magic name capitalized is CatPurComponent
+    In the angular_magic_component there are string between single square, example [form]. this is the key. And string between double square, example [[import {{FormControl}} from '@angular/forms']]. this is the code.
+    If a key is among magic_keys, remove the key, and leave ONLY the code without square brackets.
+    if a key isn't among magic_keys, remove the key and the code
 
-    In the angular magic component there are string between single square, example [form]. this is the key. And string between double square, example [[import {{FormControl}} from '@angular/forms']]. this is the code.
-
-    This is an example of angular magic components: 
+    This is an example of angular_magic_components: 
     '
         import {{Component}} from '@angular/core'
         [form]
         [[import {{FormControl}} from '@angular/forms']] 
 
         @Component({{
-            selector: '[prefix]-[magic name]',
+            selector: '[prefix]-[magic_name]',
             standalone: true,
             template: `
                 <p>Meoow!<\p>
             `,
             styles: ``
         }})
-        export class [magic name] {{
+        export class [magic_name_capitalized] {{
             [form]
             [[name = new FormControl('')]]
         }}
     '
 
-    if in the magic keys there is the key 'form', the result should be:
+    if in the magic_keys there is the key form, the result should be:
     '
         import {{Component}} from '@angular/core'
         import {{FormControl}} from '@angular/forms'
@@ -62,14 +63,14 @@ def angular_magic_component_factory(input):
         export class CatComponent {{}}
     '
 
-    This is the angular magic component: {angular_magic_component}.
+    This is the angular magic component: {angular_magic_component_template}.
 
     Return ONLY the magic component WITHOUT explanation.
     NEVER add code that is not in angular magic component
     '''
 
 
-angular_magic_component = f'''
+angular_magic_component_template = f'''
 import {{Component [signal][[signal]]}} from '@angular/core'
 
 @Component({{
